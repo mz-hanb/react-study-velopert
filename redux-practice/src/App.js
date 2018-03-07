c51a17d3753912f2d3f03d2ac43cdff015dddcf7
mz-hanb <hanb@mz.co.kr>
2018-03-07 16:24:34 +0900

    add complete redux practice

--- redux-practice/src/App.js
+++ redux-practice/src/App.js
@@ -1,18 +1,16 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Counter from './components/Counter';
+import Buttons from './components/Buttons';
+import Option from './components/Option';
 
-class App extends Component {
+class App extends Component {  
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div style={ {textAlign: 'center'} }>
+        <Counter />
+        <Option />
+        <Buttons />
       </div>
     );
   }
--- redux-practice/src/index.js
+++ redux-practice/src/index.js
@@ -4,5 +4,17 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import counterApp from './reducers';
+
+const store = createStore(counterApp);
+
+// ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render( 
+  <Provider store={store}>
+    <App />
+  </Provider>, document.getElementById('root')
+);
+
 registerServiceWorker();
